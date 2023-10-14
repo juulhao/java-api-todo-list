c6c86fccf3e224a2d27c0d95bd8068951a3a4195
Julio Rodrigues <juulhao@gmail.com>
2023-10-14 01:30:47 -0300

    Uploading Structure
    :)

---
+++ src/main/java/br/com/juliorodrigues/todolist/task/TaskModel.java
@@ -0,0 +1,46 @@
+package br.com.juliorodrigues.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+//@TODO Mudar para uma pasta de ENUMS
+enum Level {
+    LOW, MEDIUM, HIGH
+}
+
+@Data
+@Entity(name = "tab_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private Level priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title não pode ter mais do que 50 caracteres.");
+        }
+        this.title = title;
+
+    }
+
+}
